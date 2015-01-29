caaa5d741fb8893e41fa87055c95f17b487ac7df
cjrisi88 <cjrisi88@gmail.com>
2015-01-28 22:09:57 -0500

    Fixed Axa Load View

--- axa_load_view.R
+++ axa_load_view.R
@@ -1,24 +1,36 @@
 # Project: AXA Kaggle Trip Driver Predicition
 ## Fuction: read_trips
 ## Title: Driver Trip Reader
-## Creator: J.McLaren 
+## Creator: J.McLaren
 ## Create Date: January  6, 2014
-## Modifier: Christopher Risi 
-## Modified Date: January, 12, 2015
-## Version 0.2
+## Modifier: Christopher Risi
+## Modified Date: January, 28, 2015
+## Version 0.3
 ## Version Revision Notes
 ### Version 0.2 Revisions
 ### Modified this function so that it will work on anyone's working driectory
-### The working directory needs to contain at least the driver you are trying to use 
+### The working directory needs to contain at least the driver you are trying to use
 ### the fucntion for, and the driver folder must contain at least one trip.
+###Version 0.3 Revisions
+###Found a huge bug in the first trip loaded for a driver!  It copies in the
+###Same trip twice.  Applied fix so that this no longer happens.
 
-read_trips <- function(driver) 
+read_trips <- function(driver)
 {
   workDir <- getwd()
 	print(paste("Driver:",driver))
 	driver_files <- list.files(path = paste(workDir,driver,sep="/"))
 	for (file in driver_files)
 		{
+	  # if the merged dataset does exist, append to it
+	  if (exists("dataset"))
+	  {
+	    temp_dataset <-read.csv(paste(workDir,driver,file,sep="/"))
+	    temp_dataset$trip <- strsplit(file,"[.]")[[1]][1]
+	    dataset<-rbind(dataset, temp_dataset)
+	    rm(temp_dataset)
+	  }
+
 		# if the merged dataset doesn't exist, create it
 		if (!exists("dataset"))
 			{
@@ -27,14 +39,7 @@ read_trips <- function(driver)
 			dataset$trip <- strsplit(file,"[.]")[[1]][1]
 			}
 
-		# if the merged dataset does exist, append to it
-		if (exists("dataset"))
-			{
-			temp_dataset <-read.csv(paste(workDir,driver,file,sep="/"))
-			temp_dataset$trip <- strsplit(file,"[.]")[[1]][1]
-			dataset<-rbind(dataset, temp_dataset)
-			rm(temp_dataset)
-			}
+
 		}
 
 dataset$driver <- driver
